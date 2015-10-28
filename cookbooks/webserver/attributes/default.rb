a6ff9f02d98a0b1e3f6579ae9cd9de16847d8e0e
Anders Quist <anders@qinfo.se>
2015-10-28 10:46:28 +0100

    Initial commit

---
+++ cookbooks/webserver/attributes/default.rb
@@ -0,0 +1,2 @@
+default["iis"]["sites"]["clowns"] = { "port" => 80 }
+default["iis"]["sites"]["bears"] = { "port" => 81 }
---
+++ cookbooks/webserver/recipes/default.rb
@@ -0,0 +1,67 @@
+#
+# Cookbook Name:: webserver
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+# Enable the IIS role.
+dsc_script 'Web-Server' do
+  code <<-EOH
+  WindowsFeature InstallWebServer
+  {
+    Name = "Web-Server"
+    Ensure = "Present"
+  }
+  EOH
+end
+
+# Install the IIS Management Console.
+dsc_script 'Web-Mgmt-Console' do
+  code <<-EOH
+  WindowsFeature InstallIISConsole
+  {
+    Name = "Web-Mgmt-Console"
+    Ensure = "Present"
+  }
+  EOH
+end
+
+# Remove the default web site.
+include_recipe 'iis::remove_default_site'
+
+# Iterate over attributes
+node["iis"]["sites"].each do |site_name, site_data|
+  site_directory = "C:\\inetpub\\sites\\#{site_name}"
+
+  # Create the site directory and give IIS_IUSRS read rights.
+  directory site_directory do
+    rights :read, 'IIS_IUSRS'
+    recursive true
+    action :create
+  end
+
+  # Create the app pool.
+  iis_pool "#{site_name}" do
+    runtime_version '4.0'
+    action :add
+  end
+
+  # Create the site.
+  iis_site "#{site_name}" do
+    protocol :http
+    port site_data["port"]
+    path site_directory
+    application_pool "#{site_name}"
+    action [:add, :start]
+  end
+
+  # Add a template resource for Default.htm
+  template "#{site_directory}\Default.htm" do
+    source "Default.htm.erb"
+    variables(
+      :site_name => site_name,
+      :port => site_data["port"]
+    )
+  end
+end
+
